593e44aa5f8514de62c39d28d964a47beb771f03
AlexCsanyi <alex.csanyi@gmail.com>
2020-11-22 17:02:13 +0000

    Fix link

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -26,7 +26,7 @@ export default function Dashboard() {
                     <h2 className="text-center mb-4">Profile</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
                     <strong>Email: {currentUser.email}</strong>
-                    <Link to="update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
+                    <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
                 </Card.Body>
                 <div className="w-100 text-center mt-2">
                     <Button variant="link" onClick={handleLogout}>Log Out</Button>
